7a95eb064349a68103865767bd3ea0f823a516df
OMURA TORU <toru.omura@linecorp.com>
2017-06-26 15:12:35 +0900

    First Commit

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    private bool _startRunning;
+    private float _startEulerY;
+
+    void Start()
+    {
+        _startEulerY = Random.Range(0, 360);
+        this.transform.rotation = Quaternion.Euler(0, _startEulerY ,0);
+    }
+
+	void Update ()
+    {
+        if (_startRunning) {
+            this.transform.rotation = Quaternion.Euler(0, _startEulerY, 0);
+            this.transform.Translate(this.transform.forward * 0.001f, Space.World);
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor") {
+            _startRunning = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class Spawner : MonoBehaviour
+{
+	public GameObject catPrefab;
+	public float createHeight;
+	private MaterialPropertyBlock _props;
+
+	void Start()
+	{
+    	_props = new MaterialPropertyBlock();
+	}
+
+	void CreateCat(Vector3 position)
+	{
+	    Instantiate(catPrefab, position, Quaternion.identity);
+	}
+
+	void Update()
+	{
+		if (Input.touchCount > 0 )
+		{
+			var touch = Input.GetTouch(0);
+			if (touch.phase == TouchPhase.Began)
+			{
+				var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+
+				ARPoint point = new ARPoint {
+					x = screenPosition.x,
+					y = screenPosition.y
+			    };
+
+				List<ARHitTestResult> hitResults = UnityARSessionNativeInterface.GetARSessionNativeInterface().HitTest (point, 
+				ARHitTestResultType.ARHitTestResultTypeExistingPlaneUsingExtent);
+                
+			    if (hitResults.Count > 0)
+                {
+				    foreach (var hitResult in hitResults)
+                    {
+				        Vector3 position = UnityARMatrixOps.GetPosition (hitResult.worldTransform);
+				        CreateCat (new Vector3 (position.x, position.y + createHeight, position.z));
+				        break;
+				    }
+				}
+			}
+		}
+	}
+}
